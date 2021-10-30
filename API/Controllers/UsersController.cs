b191a8ecefaa74fa5b0ccb7be85ea2dd0dc695b3
jeanesterhuyse <jeanesterhuyse01@gmail.com>
2021-10-30 14:32:27 +0200

    Created basic layout of API and user controller.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;
+        public UsersController(DataContext context)
+        {
+            this.context=context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users=this.context.Users.ToList();
+
+            return users;
+        }
+
+         [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return this.context.Users.Find(id);
+            
+            
+        }
+    
+    }
+}
