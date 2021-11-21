e7b0643a008e9f5742e9aeec325904b3ef90f938
Noddy30 <neduan41@gmail.com>
2021-11-21 09:59:16 +0200

    Error fixed
    Co-Authored-By: Noddy30 <70162357+Noddy30@users.noreply.github.com>

--- API/DTOs/PhotoDto.cs
+++ API/DTOs/PhotoDto.cs
@@ -4,7 +4,7 @@ namespace API.DTOs
     public class PhotoDto
     {
         public int id { get; set; }
-        //public int foldersId { get; set; }
+        public int foldersId { get; set; }
         public string url { get; set; }
         public bool isMain { get; set; }
         public ICollection<MetaDataDto> MetaData {get; set;}
